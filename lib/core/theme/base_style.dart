90282e2b3317f09a19fb413e86366f66befd9349
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2023-03-11 05:06:11 +0700

    refactor: organize project

---
+++ lib/core/theme/base_style.dart
@@ -0,0 +1,119 @@
+import "package:flutter/material.dart";
+
+class BaseColor {
+  static const Color black = Color(0xFF222222);
+  static const Color hint = Color(0xFFA3A3A3);
+  static const Color lightGrey = Color(0xFFF3F3F3);
+  static const Color blue = Color(0xFF003399);
+  static const Color secondaryBlue = Color(0xFFE5EEFF);
+  static const Color off = Color(0xFFC3CAE9);
+  static const Color orange = Color(0xFFFF9900);
+  static const Color secondaryOrange = Color(0xFFFFF5E5);
+  static const Color red = Color(0xFFD43513);
+  static const Color secondaryRed = Color(0xFFFBEAE9);
+  static const Color green = Color(0xFF00C11F);
+  static const Color secondaryGreen = Color(0xFFE9FCEC);
+}
+
+class BaseTextStyle {
+  static const appFontFamily = "Roboto";
+
+  static TextStyle heading1({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 32,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w400);
+  }
+
+  static TextStyle heading2({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 24,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w500);
+  }
+
+  static TextStyle heading3({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 24,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w400);
+  }
+
+  static TextStyle heading4({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 20,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w600);
+  }
+
+  static TextStyle heading5({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 20,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w400);
+  }
+
+  static TextStyle subtitle1({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 18,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w500);
+  }
+
+  static TextStyle subtitle2({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 16,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w500);
+  }
+
+  static TextStyle subtitle3({Color? color, double? fontSize}) {
+    return TextStyle(
+        decoration: TextDecoration.underline,
+        fontSize: fontSize ?? 16,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w500);
+  }
+
+  static TextStyle body1({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 18,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w400);
+  }
+
+  static TextStyle body2({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 16,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w400);
+  }
+
+  static TextStyle body3({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 14,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w500);
+  }
+
+  static TextStyle appBar({Color? color, double? fontSize,FontWeight? fontWeight}) {
+    return TextStyle(
+        fontSize: fontSize ?? 14,
+        color: color ?? BaseColor.black,
+        fontWeight: fontWeight ?? FontWeight.w800,);
+  }
+
+  static TextStyle button({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 16,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w600);
+  }
+
+  static TextStyle caption({Color? color, double? fontSize}) {
+    return TextStyle(
+        fontSize: fontSize ?? 12,
+        color: color ?? BaseColor.black,
+        fontWeight: FontWeight.w500);
+  }
+}
---
+++ lib/core/theme/base_theme.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+
+ThemeData baseTheme() {
+  final ThemeData base = ThemeData(
+    backgroundColor: Colors.white,
+    dividerColor: BaseColor.hint,
+    errorColor: BaseColor.red,
+    focusColor: BaseColor.blue,
+    hintColor: BaseColor.hint,
+    primaryColor: BaseColor.blue,
+  );
+  return base;
+}
+
+
+class BaseColor {
+  static const Color black = Color(0xFF222222);
+  static const Color hint = Color(0xFFA3A3A3);
+  static const Color lightGrey = Color(0xFFF3F3F3);
+  static const Color blue = Color(0xFF003399);
+  static const Color secondaryBlue = Color(0xFFE5EEFF);
+  static const Color off = Color(0xFFC3CAE9);
+  static const Color orange = Color(0xFFFF9900);
+  static const Color secondaryOrange = Color(0xFFFFF5E5);
+  static const Color red = Color(0xFFD43513);
+  static const Color secondaryRed = Color(0xFFFBEAE9);
+  static const Color green = Color(0xFF00C11F);
+  static const Color secondaryGreen = Color(0xFFE9FCEC);
+}
