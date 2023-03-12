199ed46af48ce8eb33948d978591b138aa781413
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2023-03-12 20:50:40 +0700

    feat: update course detail screen

--- lib/app/modules/course_detail/course_detail.dart
+++ lib/app/modules/course_detail/course_detail.dart
@@ -28,20 +28,96 @@ class CourseDetail extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(18),
           child: Column(
-            children: const [
-              CourseCardExplore(),
+            children: [
+              const CourseCardExplore(),
               sh_20,
-              CourseDescriptionHeader(description: "Overview"),
+              const CourseDescriptionHeader(description: "Overview"),
+              const CourseAttribute(
+                title: "Why take this course",
+                description: lorem,
+                icon: questionMaskIcon,
+              ),
               sh_20,
-              CourseDescriptionHeader(description: "Experience Level"),
+              const CourseDescriptionHeader(description: "Experience Level"),
+              const CourseAttribute(
+                title: "Beginner",
+                description: "This course for noob",
+                icon: peopleIcon,
+              ),
               sh_20,
-              CourseDescriptionHeader(description: "Course Length"),
+              const CourseDescriptionHeader(description: "Course Length"),
+              const CourseAttribute(
+                title: "6 Topic",
+                description: "Sleep now",
+                icon: docIcon,
+              ),
               sh_20,
-              CourseDescriptionHeader(description: "List Topics"),
+              const CourseDescriptionHeader(description: "List Topics"),
+              sh_10,
+              ...topics
+                  .asMap()
+                  .entries
+                  .map((entry) => Card(
+                        elevation: 2,
+                        child: Container(
+                          height: 100,
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("${entry.key}.",
+                                  style: BaseTextStyle.body2()),
+                              Text("${entry.value}.",
+                                  style: BaseTextStyle.body2()),
+                            ],
+                          ),
+                        ),
+                      ))
+                  .toList()
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class CourseAttribute extends StatelessWidget {
+  const CourseAttribute({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.icon,
+  });
+
+  final String title;
+  final String description;
+  final Widget icon;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            icon,
+            const SizedBox(
+              width: 3,
+            ),
+            Text(
+              title,
+              style: BaseTextStyle.subtitle2(),
+            ),
+          ],
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: double.infinity,
+          child: Text(description),
+        )
+      ],
     );
   }
 }
@@ -66,3 +142,40 @@ class CourseDescriptionHeader extends StatelessWidget {
     );
   }
 }
+
+const questionMaskIcon = Material(
+  color: Colors.transparent,
+  shape: CircleBorder(side: BorderSide(color: Colors.red)),
+  child: Icon(
+    size: 20,
+    Icons.question_mark_rounded,
+    color: Colors.red,
+  ),
+);
+
+const peopleIcon = Material(
+  color: Colors.transparent,
+  child: Icon(
+    size: 20,
+    Icons.people_alt_rounded,
+    color: Colors.blue,
+  ),
+);
+
+const docIcon = Material(
+  color: Colors.transparent,
+  child: Icon(
+    size: 20,
+    Icons.document_scanner_rounded,
+    color: Colors.blue,
+  ),
+);
+
+const List<String> topics = [
+  'I want to sleep',
+  'I want to sleep 2',
+  'I want to sleep 3'
+];
+
+const lorem =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco';
